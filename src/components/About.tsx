9d7adde8e9d7f3a4bf274a908c0c119a0d56d1e0
Panisara kanjanachotkamol <6633150721@student.chula.ac.th>
2024-03-26 00:47:45 +0700

    about us not completed but sleepy mak

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,23 @@
 export default function About() {
   return (
-    <div className=" px-10 h-[190vh]">
-    <div className="text-5xl font-extrabold text-black text-end" id="aboutus">
-      ABOUT US
-    </div>
+    <div className=" py-10 ">
+      <div className="text-5xl font-extrabold text-black text-end p-5 " id="aboutus">
+        ABOUT US
+      </div>
+      <div className="flex flex-col text-center">
+        <div className="bg-black  flex flex-col h-[25vh] 5xl bg-black text-white rounded-[8vh] ">
+          <div className="text-4xl p-3 text-white">Varinthorn Chatburapachai ( Anda )</div>
+          <div className="text-xl text-white font-extralight">id : 6633221621</div>
+        </div>
+        <div className="bg-black  flex flex-col h-[25vh] 5xl bg-black text-white rounded-[8vh]">
+          <div className="text-4xl p-3 text-white">Panisara kanjanachotkamol ( Model )</div>
+          <div className="text-xl text-white font-extralight">id : 6633150721</div>
+        </div>
+        <div className="bg-black  flex flex-col h-[25vh] 5xl bg-black text-white rounded-[8vh]">
+          <div className="text-4xl p-3 text-white">Thawaree Khanjai ( Kaning )</div>
+          <div className="text-xl text-white font-extralight">id : 6633084021</div>
+        </div>
+      </div>
     </div>
   );
 }
