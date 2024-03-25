a233bf48556374b0c2eb491c360344c553e5e6b0
NaMMly YuMMy <andavarinthorn1409@gmail.com>
2024-03-25 23:54:23 +0700

    add Feedbacks

--- src/components/Feedbacks.tsx
+++ src/components/Feedbacks.tsx
@@ -6,10 +6,8 @@ import { motion } from "framer-motion";
 
 export default function FeedBacks() {
   return (
-    <div
-      className="mt-[30vh] mx-[5vw] 5xl bg-black flex text-white h-[200vh] justify-center items-center rounded-[5vh]"
-      id="feedbacks"
-    >
+    <div className="mt-[30vh] mx-[5vw] 5xl bg-black flex text-white h-[200vh] justify-center items-center rounded-[5vh]">
+      <div className="mt-[-150vh]" id="feedbacks"></div>
       <div className="bg-black w-[55%] h-[25vh] absolute rounded-[5vh] top-[420vh] left-[5vw] text-center text-white flex flex-col pt-10">
         <div className="text-6xl font-extrabold">CUSTOMER FEEDBACKS</div>
       </div>
