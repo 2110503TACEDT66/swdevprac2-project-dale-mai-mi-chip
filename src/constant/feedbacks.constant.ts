b0fe674254ec5e8d5c45199284645bbac0a55c63
Thawaree Khanjai <1042kaning1042@gmail.com>
2024-03-26 22:41:41 +0700

    add github link in main page

--- src/constant/feedbacks.constant.ts
+++ src/constant/feedbacks.constant.ts
@@ -1,8 +1,9 @@
 export const allFeedBacks: { name: string; path: string; scroll : string }[] = [
-    { name: "Kristine Haley shop", path: "public/img/back.png", scroll : "center" },
+    { name: "Kristine Haley shop", path: "#ourservices", scroll : "center" },
     { name: "Garry Hahn shop", path: "#funfacts", scroll : "center" },
     { name: "Salvador Dickinson shop", path: "#feedbacks", scroll : "center" },
     { name: "Dr. Joshua Nader shop", path: "#myreservation", scroll : "center" },
     { name: "May Hilpert Jr. shop", path: "#aboutus", scroll : "center"},
     { name: "Fredrick Konopelski shop", path: "#aboutus", scroll : "center"},
   ];
+
